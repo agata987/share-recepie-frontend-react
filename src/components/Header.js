ad04fa5bcd3faaec9ac6d1ae1a1feccfb493a9e9
agata <agata.szczypinska@gmail.com>
2021-02-06 18:24:01 +0100

    search input styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import SearchInput from './SearchInput'
+import iconSet from '../svg/selection.json';
+import IcomoonReact from 'icomoon-react';
 
 const Header = (props) => {
-  return <div>header</div>;
+  return (
+    <header id="main-header">
+      <IcomoonReact id="logo" iconSet={iconSet} icon='leaf' />
+      <SearchInput />
+      <div>login</div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/SearchInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import iconSet from '../svg/selection.json';
+import IcomoonReact from 'icomoon-react';
+
+const SearchInput = (props) => {
+  return (
+    <div className='search'>
+      <input
+        className="search__input"
+        type="text"
+        placeholder="Search..."
+      />
+      <button className='search__button'>
+        <IcomoonReact className="icon" iconSet={iconSet} icon="search" />
+      </button>
+    </div>
+  );
+};
+
+export default SearchInput;
