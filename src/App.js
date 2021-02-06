1136df5a19272da6688d17ce4a09fa9bd8bd9259
agata <agata.szczypinska@gmail.com>
2021-02-06 16:40:33 +0100

    basic styles & main container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from './components/Header';
+import Navbar from './components/NavBar';
+import RecepiesView from './components/views/RecepiesView';
+
 function App() {
   return (
-    <div className="App">
-      costam 
+    <div id="main-container">
+      <Header />
+      <Navbar />
+      <RecepiesView />
     </div>
   );
 }
---
+++ src/components/views/RecepiesView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecepiesView = (props) => {
+  return <div>recepies</div>;
+};
+
+export default RecepiesView;
